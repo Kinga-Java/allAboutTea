ef8dff26c915ae747cd1d5fbd0698599e732efe5
Kinga-Java <kinga.gozdziuk@gmail.com>
2020-09-19 23:24:06 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/allabouttea/AllaboutteaApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.allabouttea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaboutteaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllaboutteaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/allabouttea/AllaboutteaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.allabouttea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllaboutteaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
